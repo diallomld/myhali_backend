44a580e50b41649c0fd9958a13abe6b821395e42
Lamine <diallomld5@gmail.com>
2026-04-08 05:06:55 +0200

    update requirement

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn myhali_backend.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -3,6 +3,6 @@ from django.core.wsgi import get_wsgi_application
 # Pas besoin d'importer WhiteNoise ici si tu l'as mis dans MIDDLEWARE (recommandé)
 
 # Assure-toi que le nom correspond bien à ton dossier principal
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myhali_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
