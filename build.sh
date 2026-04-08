d55f6a2ff3d10004c47267666ca53d85f7aca93d
Lamine <diallomld5@gmail.com>
2026-04-08 06:46:23 +0200

    update requirement

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+DJANGO_SUPERUSER_PASSWORD=Passer@123
 python manage.py createsuperuser \
     --no-input \
-    --username "$DJANGO_SUPERUSER_USERNAME" \
-    --email "$DJANGO_SUPERUSER_EMAIL" || true
+    --username admin2 \
+    --email admin@test.com
